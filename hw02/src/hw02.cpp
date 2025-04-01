40484adf44f7d734b8c45a519a3ad17d9daea27f
ggrraachh <yaroslav.efrem@yandex.ru>
2025-04-01 12:17:23 +0300

    add hw02

---
+++ hw02/src/hw02.cpp
@@ -0,0 +1,26 @@
+#include <hw02.h>
+#include <iostream>
+#include <string>
+using namespace std; 
+
+string hw02(std::string input_str)
+{
+    size_t i = 0;
+    while (i < input_str.size()) {
+        char current = input_str[i];
+        if (input_str.substr(0, i).find(current) != string::npos) {
+            input_str.erase(i, 1); 
+        } else {
+            ++i; 
+        }
+    }
+    return input_str;
+}
+
+int main() {
+    cout << hw02("Hello") << endl;
+    cout << hw02("AABBCC") << endl;
+    cout << hw02("!HHWW02COMPLLETEE!") << endl;
+    return 0;
+}
+
